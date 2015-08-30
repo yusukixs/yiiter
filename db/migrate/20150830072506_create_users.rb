4e1bdd4163456a5df0f124eba5aa7f7b2dcc0aa8
yusukixs <yusukixs@gmail.com>
2015-08-31 08:59:09 +0900

    Userモデルを作成

---
+++ db/migrate/20150830072506_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :account_name, null: false
+      t.string :full_name, null: false
+      t.string :email
+      t.boolean :administrator, null: false, default: false
+
+      t.timestamps null: false
+    end
+  end
+end
