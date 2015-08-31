6269c2a7de52bec06d034ede1bcb8fd691101115
yusukixs <yusukixs@gmail.com>
2015-08-31 14:26:15 +0900

    トップページのレイアウトを修正。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     title = @page_title + " | " + title if @page_title
     title
   end
+  
+  def menu_link_to(text, path)
+    link_to_unless_current(text, path) {content_tag(:span, text)}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "top#index"
+  
+  get "about" => "top#about", as: "about"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
