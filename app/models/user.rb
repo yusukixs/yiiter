d409f079f17bdef9e31ace01cd2536f0e180379b
yusukixs <yusukixs@gmail.com>
2015-09-02 13:05:27 +0900

    ログインパスワードを暗号化

---
+++ app/models/user.rb
@@ -0,0 +1,37 @@
+class User < ActiveRecord::Base
+  validates :account_name,
+    presence: true,
+    uniqueness: { case_sensitive: false },
+    length: { maximum: 16 },
+    format: { with: /\A[a-z0-9]+\z/i, message: :invalid_account_name }
+    
+  validates :full_name,
+    presence: true,
+    length: { maximum: 20 }
+    
+  validates :password,
+    presence: { on: :create },
+    length: { minimum: 6 },
+    confirmation: { allow_blank: true }
+    
+  attr_accessor :password, :password_confirmation
+  
+  def password=(val)
+    if val.present?
+      self.password = BCrypt::Password.create(val)
+    end
+    @password = val
+  end
+  
+  class << self
+    def authenticate(account_name, password)
+      user = find_by(account_name: account_name)
+      if user && user.password.present? &&
+        BCrypt::Password.new(user.password) == password
+        user
+      else
+        nil
+      end
+    end
+  end
+end
