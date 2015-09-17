13db5d98ca5836826bd74b049acadb2e497f81b8
yusukixs <yusukixs@gmail.com>
2015-09-17 16:54:23 +0900

    Markdownの表示オプションを変更。

--- db/seeds/development/articles.rb
+++ db/seeds/development/articles.rb
@@ -1,4 +1,23 @@
-text = "あいうえお¥n- 箇条書き1¥n  - 箇条書き2"
+text = "
+* リスト1
+  * リスト1-1(入れ子もOK)
+  * リスト1-2(入れ子もOK)
+* リスト2
+
+1. 数字リスト1
+  1. 数字リスト1-1(入れ子もOK)
+  1. 数字リスト1-2(入れ子もOK)
+1. 数字リスト2
+
+## Rubyコード
+
+~~~ ruby
+test = [ 10, 20 ]
+test.each do |v|
+  puts v
+end
+~~~
+"
 
 %w(Taro Jiro Hana).each do |name|
   user = User.find_by(account_name: name)
@@ -19,11 +38,15 @@ text = "あいうえお¥n- 箇条書き1¥n  - 箇条書き2"
     if idx == 4 || idx == 8
       %w(John Mike Sophy).each do |name3|
         commenter = User.find_by(account_name: name3)
-        #commenter.commented_articles << article
         Comment.create(
           user: commenter,
           article: article,
-          comment: "###あいうえお#{idx}"
+          comment: "引用1
+
+> > 引用2 (入れ子にも対応)
+
+> 引用3  
+> 引用元: <cite>さかなチキンぱん。</cite>"
         ) 
       end
     end
